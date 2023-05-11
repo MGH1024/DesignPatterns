1e8a366d84a3eed2cdda57821578a9f4d26b83a7
Meysam <Meysam@MGH>
2023-05-11 17:07:56 +0330

    template pattern

--- Template/Template/Program.cs
+++ Template/Template/Program.cs
@@ -8,4 +8,4 @@ Client.ClientCode(new ConcreteClass1());
 Console.Write("\n");
             
 Console.WriteLine("Same client code can work with different subclasses:");
-Client.ClientCode(new ConcreteClass2());
+Client.ClientCode(new ConcreteClass2()); 
