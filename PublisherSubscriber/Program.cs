8be0b22ecb29a441a4fef9a078458e7cf7932586
MGH1024 <meysam.ghiasvand@gmail.com>
2024-12-05 11:06:07 +0330

    add pub/sub pattern

---
+++ PublisherSubscriber/Program.cs
@@ -0,0 +1,21 @@
+﻿using PublisherSubscriber;
+
+WeatherStation weatherStation = new WeatherStation();
+
+// Create subscribers
+WeatherDisplay display1 = new WeatherDisplay("Display 1");
+WeatherDisplay display2 = new WeatherDisplay("Display 2");
+
+// Subscribe to the event
+weatherStation.WeatherChanged += display1.OnWeatherChanged;
+weatherStation.WeatherChanged += display2.OnWeatherChanged;
+
+// Update weather
+weatherStation.UpdateWeather("Sunny");
+weatherStation.UpdateWeather("Rainy");
+
+// Unsubscribe one of the displays
+weatherStation.WeatherChanged -= display2.OnWeatherChanged;
+
+// Update weather again
+weatherStation.UpdateWeather("Cloudy");
